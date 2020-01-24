8f3ec11161678f43fb1f7d1fb5006753045bf45a
Adam G Horn <hornadam8@gmail.com>
2020-01-24 01:58:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-  if array.len
+  if array.length == 2
+    array.join(" and ")
+  elsif array.length == 1
+    array.join
+  elsif array.length > 2
+    array[-1].prepend(" and ")
 end
